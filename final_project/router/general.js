67e02e9f2477010cc55522260d0c68b61b798cb0
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-03-14 16:18:33 +0530

    Implement book review retrieval endpoint by ISBN

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -62,8 +62,16 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  
+  if (books[isbn]) {
+    res.setHeader('Content-Type', 'application/json');
+    return res.status(200).send(JSON.stringify({
+      isbn: isbn,
+      reviews: books[isbn].reviews
+    }, null, 2));
+  }
+  return res.status(404).json({message: `Book with ISBN ${isbn} not found`});
 });
 
 module.exports.general = public_users;
